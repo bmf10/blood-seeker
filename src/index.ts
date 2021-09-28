075a729e191eb727e57963e38e8209cf52949151
BmF10 <bimafebriansyah1002@gmail.com>
2021-09-28 10:04:36 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { json, urlencoded } from 'express'
+import db from './models'
+
+const app = express()
+
+const PORT = parseInt(process.env.PORT || '5000')
+
+app.use(async (req, res, next) => {
+    await db.sequelize.authenticate()
+    next()
+})
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH')
+    next()
+})
+
+app.use(json())
+app.use(urlencoded({ extended: false }))
+
+app.get('/ping', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`listening at port ${PORT}`)
+})
---
+++ src/model-type.ts
@@ -0,0 +1,34 @@
+import Database = require('sequelize')
+import type { Sequelize, DataTypes, ModelType } from 'sequelize'
+
+export interface Config {
+    readonly dialect: string
+    readonly storage: string
+}
+
+export interface ConfigMap {
+    readonly [k: string]: Config
+}
+
+export interface AssociateFunction {
+    (db: Db): void
+}
+
+export interface AssociatedModel extends ModelType {
+    readonly associate?: AssociateFunction
+}
+
+export interface ModelMap {
+    [key: string]: unknown
+}
+
+export interface Db {
+    readonly Sequelize: typeof Database
+    readonly sequelize: Sequelize
+    readonly models: Readonly<ModelMap>
+}
+
+export interface DefineFunction {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (sequelize: Sequelize, dataTypes: typeof DataTypes): any
+}
